5eacde996858326ab8ce22156174606d3140f278
yassirkibou06 <yassirkibou06@gmail.com>
2023-05-24 20:19:46 +0100

    add

---
+++ script.js
@@ -0,0 +1,12 @@
+import lottieWeb from 'https://cdn.skypack.dev/lottie-web';
+
+var animation = lottieWeb.loadAnimation({
+  container: document.getElementById('anim'),
+  path: 'https://assets9.lottiefiles.com/packages/lf20_yb9dhl1b.json',
+  renderer: 'svg',
+  loop: false,
+  autoplay: false,
+  name: "Demo Animation",
+});
+
+
